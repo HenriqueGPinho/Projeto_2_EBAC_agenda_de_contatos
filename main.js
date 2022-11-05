3616f65a16d0892b6f507ed6a4941b740b2a769b
Henrique Pinho <henriquegravino@outlook.com>
2022-11-05 13:46:28 -0300

    funcionalidades básicas

---
+++ main.js
@@ -0,0 +1,23 @@
+let nomeContato = document.getElementById("nome-contato")
+let numeroContato = document.getElementById("numero-contato")
+let botaoSalvar = document.getElementById("salvar-contato")
+let tabela = document.getElementById("tabela-contatos")
+
+botaoSalvar.addEventListener("click", e => {
+  e.preventDefault()
+
+  tabela.innerHTML += montarLinhaTabela(nomeContato.value, numeroContato.value)
+
+
+})
+
+function montarLinhaTabela(nome, contato) {
+  return (
+    `
+      <tr>
+        <td>${nome}</td>
+        <td>${contato}</td>
+      </tr>
+    `
+  )
+}
