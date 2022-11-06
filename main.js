320ea668c1396fdf2828a3ba06da48bf6b1ea397
Henrique Pinho <henriquegravino@outlook.com>
2022-11-05 22:22:03 -0300

    fix: form validation

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-let nomeContato = document.getElementById("nome-contato")
-let numeroContato = document.getElementById("numero-contato")
-let botaoSalvar = document.getElementById("salvar-contato")
-let tabela = document.getElementById("tabela-contatos")
+let nomeContato = document.getElementById('nome-contato')
+let numeroContato = document.getElementById('numero-contato')
+let botaoSalvar = document.getElementById('salvar-contato')
+let tabela = document.getElementById('tabela-contatos')
 
-botaoSalvar.addEventListener("click", e => {
-  e.preventDefault()
+botaoSalvar.addEventListener('click', () => {
 
-  tabela.innerHTML += montarLinhaTabela(nomeContato.value, numeroContato.value)
+  if (nomeContato.value === '' || numeroContato.value === '') {
+    alert('Preencha os dois campos do formulário!')
+  } else {
+    tabela.innerHTML += montarLinhaTabela(nomeContato.value, numeroContato.value)
+  
+    nomeContato.value = ''
+    numeroContato.value = ''
+  }
+  //TODO: apagar os campos depois de clicar em salvar, mudar o campo de número para texto
 })
 
 function montarLinhaTabela(nome, contato) {
